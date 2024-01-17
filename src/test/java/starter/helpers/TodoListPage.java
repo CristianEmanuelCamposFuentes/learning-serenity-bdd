13ec2474a77a23ad9b82cf1a916c4b50370292f9
Cristian Emanuel Campos Fuentes <cristianemanuelcamposfuentes@hotmail.com>
2024-01-16 23:30:24 -0300

    Add new DefaultUrl

--- src/test/java/starter/helpers/TodoListPage.java
+++ src/test/java/starter/helpers/TodoListPage.java
@@ -4,7 +4,7 @@ import net.serenitybdd.annotations.DefaultUrl;
 import net.serenitybdd.screenplay.targets.Target;
 import net.serenitybdd.core.pages.PageObject;
 
-@DefaultUrl("https://todomvc.com/examples/angularjs/#/")
+@DefaultUrl("https://todomvc.com/examples/angular/dist/browser/#/all")
 public class TodoListPage extends PageObject {
 
         public static Target ITEM_NAME_FIELD = Target.the("item name field").locatedBy(".new-todo");
