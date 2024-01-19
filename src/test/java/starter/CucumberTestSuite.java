0f55118e566d68bb0b0a528f43e1cd31ba4a8930
Cristian Emanuel Campos Fuentes <cristianemanuelcamposfuentes@hotmail.com>
2024-01-18 21:11:36 -0300

    Add explanatory comments

--- src/test/java/starter/CucumberTestSuite.java
+++ src/test/java/starter/CucumberTestSuite.java
@@ -1,3 +1,4 @@
+// CucumberTestSuite.java
 package starter;
 
 import org.junit.platform.suite.api.ConfigurationParameter;
@@ -7,9 +8,15 @@ import org.junit.platform.suite.api.Suite;
 
 import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
+// Esta anotación marca la clase como una suite de pruebas que utiliza el motor de ejecución de Cucumber.
 @Suite
+// Indica que se deben incluir los motores especificados en la ejecución de la suite.
 @IncludeEngines("cucumber")
+// Especifica la ubicación de los archivos de características de Cucumber.
 @SelectClasspathResource("/features")
+// Configura la suite con parámetros, como el plugin de informes de Serenity y otros parámetros de ejecución.
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "io.cucumber.core.plugin.SerenityReporterParallel,pretty,timeline:build/test-results/timeline")
 public class CucumberTestSuite {
+    // Esta clase no requiere más lógica, ya que las anotaciones proporcionan la configuración necesaria.
 }
+
