54c2ccfe9d933ea4970ca01cffd233aa19dc14c0
Cristian Emanuel Campos Fuentes <cristianemanuelcamposfuentes@hotmail.com>
2024-01-18 21:09:42 -0300

    Add explanatory comments

--- src/test/java/starter/helpers/AddAnItem.java
+++ src/test/java/starter/helpers/AddAnItem.java
@@ -1,3 +1,4 @@
+// AddAnItem.java
 package starter.helpers;
 
 import net.serenitybdd.screenplay.Performable;
@@ -6,10 +7,17 @@ import net.serenitybdd.screenplay.actions.Enter;
 import org.openqa.selenium.Keys;
 
 public class AddAnItem {
+
+    // Acción: El actor agrega un ítem con un nombre específico a la lista de tareas.
     public static Performable withName(String itemName) {
         return Task.where("{0} adds an item with name " + itemName,
-        Enter.theValue(itemName)
-                .into(TodoListPage.ITEM_NAME_FIELD)
-                .thenHit(Keys.ENTER));
+
+                // Acción: El actor ingresa el nombre del ítem en el campo correspondiente.
+                Enter.theValue(itemName)
+                        .into(TodoListPage.ITEM_NAME_FIELD)
+                        // Acción: El actor presiona la tecla Enter para confirmar la adición del ítem.
+                        .thenHit(Keys.ENTER)
+        );
     }
 }
+
