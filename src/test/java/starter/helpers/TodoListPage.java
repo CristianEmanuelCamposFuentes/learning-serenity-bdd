a99642d36600e3983f1f3d8b9ebabe4149341360
Cristian Emanuel Campos Fuentes <cristianemanuelcamposfuentes@hotmail.com>
2024-01-18 21:12:49 -0300

    Add explanatory comments

--- src/test/java/starter/helpers/TodoListPage.java
+++ src/test/java/starter/helpers/TodoListPage.java
@@ -1,14 +1,19 @@
+// TodoListPage.java
 package starter.helpers;
 
 import net.serenitybdd.annotations.DefaultUrl;
 import net.serenitybdd.screenplay.targets.Target;
 import net.serenitybdd.core.pages.PageObject;
 
+// Esta anotación establece la URL predeterminada para la página de la lista de tareas.
 @DefaultUrl("https://todomvc.com/examples/angular/dist/browser/#/all")
 public class TodoListPage extends PageObject {
 
+        // Elemento que representa el campo para agregar un nuevo ítem a la lista de tareas.
         public static Target ITEM_NAME_FIELD = Target.the("item name field").locatedBy(".new-todo");
 
+        // Elemento que representa la lista de ítems en la página de la lista de tareas.
         public static Target ITEMS_LIST = Target.the("item list").locatedBy(".todo-list li");
 }
+
 
