ab8cbd5a27b4cb81dc6504cc63fd94ee7ce1ce74
Cristian Emanuel Campos Fuentes <cristianemanuelcamposfuentes@hotmail.com>
2024-01-18 21:09:53 -0300

    Add explanatory comments

--- src/test/java/starter/stepdefinitions/AddItemStepDefinitions.java
+++ src/test/java/starter/stepdefinitions/AddItemStepDefinitions.java
@@ -1,3 +1,4 @@
+// AddItemStepDefinitions.java
 package starter.stepdefinitions;
 
 import io.cucumber.java.en.Given;
@@ -10,19 +11,23 @@ import starter.helpers.NavigateTo;
 import starter.helpers.TodoListPage;
 
 public class AddItemStepDefinitions {
+
+    // Paso: El actor está viendo su lista de tareas.
     @Given("{actor} is looking at his TODO list")
     public void actor_is_looking_at_his_todo_list(Actor actor) {
-        // Write code here that turns the phrase above into concrete actions
         actor.wasAbleTo(NavigateTo.theTodoListPage());
     }
+
+    // Paso: El actor agrega un ítem a la lista de tareas.
     @When("{actor} adds {string} to the list")
     public void he_adds_to_the_list(Actor actor, String itemName) {
-        // Write code here that turns the phrase above into concrete actions
         actor.attemptsTo(AddAnItem.withName(itemName));
     }
+
+    // Paso: El actor ve un ítem específico en la lista de tareas.
     @Then("{actor} sees {string} as an item in the TODO list")
     public void he_sees_as_an_item_in_the_todo_list(Actor actor, String expectedItemName) {
-        // Write code here that turns the phrase above into concrete actions
         actor.attemptsTo(Ensure.that(TodoListPage.ITEMS_LIST).hasText(expectedItemName));
     }
 }
+
